a749ced0a8668186a724c0569d785f461b76cdc5
Sudip Regmi <96606675+regcyb1@users.noreply.github.com>
2024-04-25 17:23:59 +0545

    Wow

--- Program.cs
+++ Program.cs
@@ -30,24 +30,34 @@
 // }
 
 
-Random dice=new Random();
-int roll1=dice.Next(1,6);
-int roll2=dice.Next(1,6);
-int roll3=dice.Next(1,6);
+// Random dice=new Random();
+// int roll1=dice.Next(1,6);
+// int roll2=dice.Next(1,6);
+// int roll3=dice.Next(1,6);
 
-int total=roll1 + roll2 + roll3;
-Console.WriteLine($"DiceRoll: {roll1}+{roll2}+{roll3}={total}");
+// int total=roll1 + roll2 + roll3;
+// Console.WriteLine($"DiceRoll: {roll1}+{roll2}+{roll3}={total}");
 
-if((roll1==roll2)||(roll2==roll3)||(roll1==roll3)){
-    Console.WriteLine("You rolled a Double! Total+2");
-    total+=2;
+// if((roll1==roll2)||(roll2==roll3)||(roll1==roll3)){
+//     Console.WriteLine("You rolled a Double! Total+2");
+//     total+=2;
+// }
+// if((roll1==roll2)&&(roll2==roll3)){
+//     Console.WriteLine("Your rolled  three of the same number +6 Bonus");
+//     total+=6;
+// }
+// if (total >= 15){
+//     Console.WriteLine("You Win");
+// }else{
+//     Console.WriteLine("You lose");
+//}
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum=0;
+int bin=0;
+foreach(int value in inventory){
+    sum+= value;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {value} items (Running total: {sum})");
 }
-if((roll1==roll2)&&(roll2==roll3)){
-    Console.WriteLine("Your rolled  three of the same number +6 Bonus");
-    total+=6;
-}
-if (total >= 15){
-    Console.WriteLine("You Win");
-}else{
-    Console.WriteLine("You lose");
-}
+Console.Write($"The Inventory Sum is {sum}\n");
