7f503c34f853e9c1090f7982a200f4fe7f0bb0fc
Sudip Regmi <96606675+regcyb1@users.noreply.github.com>
2024-04-25 13:26:33 +0545

    Second commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿Random dice= new Random();
-int roll=dice.Next(1,7);
-Console.WriteLine(roll);
+﻿// Random dice= new Random();
+// int roll=dice.Next(1,7);
+// Console.WriteLine(roll);
+
+
+
+
+Random num= new Random();
+int roll=num.Next();
+int roll1=num.Next(100,1000);
+int roll2=num.Next(1,7);
+
+Console.WriteLine($"first Random={roll}");
+Console.WriteLine($"Sec Random={roll1}");
+Console.WriteLine($"Th Random={roll2}");
