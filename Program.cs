4469d8b77cdfcb0df3fa1a758f7b23d53518d571
Sudip Regmi <96606675+regcyb1@users.noreply.github.com>
2024-04-25 16:08:05 +0545

    Wakka lagdo

--- Program.cs
+++ Program.cs
@@ -30,9 +30,24 @@
 // }
 
 
-string msg="I am From Nepal";
-bool result=msg.Contains("Nepal");
-Console.WriteLine(result);
-if(msg.Contains("Nepal")){
-    Console.WriteLine("Where you from");
+Random dice=new Random();
+int roll1=dice.Next(1,6);
+int roll2=dice.Next(1,6);
+int roll3=dice.Next(1,6);
+
+int total=roll1 + roll2 + roll3;
+Console.WriteLine($"DiceRoll: {roll1}+{roll2}+{roll3}={total}");
+
+if((roll1==roll2)||(roll2==roll3)||(roll1==roll3)){
+    Console.WriteLine("You rolled a Double! Total+2");
+    total+=2;
+}
+if((roll1==roll2)&&(roll2==roll3)){
+    Console.WriteLine("Your rolled  three of the same number +6 Bonus");
+    total+=6;
+}
+if (total >= 15){
+    Console.WriteLine("You Win");
+}else{
+    Console.WriteLine("You lose");
 }
